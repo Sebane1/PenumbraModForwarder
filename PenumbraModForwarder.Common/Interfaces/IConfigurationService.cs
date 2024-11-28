7449a2e13f4e9d3f4bb77c6cb5d32e63dd8e62e0
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2024-11-28 21:47:37 +0800

    Add missed interfaces

--- PenumbraModForwarder.Common/Interfaces/IConfigurationService.cs
+++ PenumbraModForwarder.Common/Interfaces/IConfigurationService.cs
@@ -1,7 +1,11 @@
-﻿namespace PenumbraModForwarder.Common.Interfaces;
+﻿using PenumbraModForwarder.Common.Models;
+
+namespace PenumbraModForwarder.Common.Interfaces;
 
 public interface IConfigurationService
 {
     public void CreateConfiguration();
     public event EventHandler ConfigurationChanged;
+    public void UpdateConfigValue(Action<ConfigurationModel> propertyUpdater);
+    public object ReturnConfigValue(Func<ConfigurationModel, object> propertySelector);
 }
