298f57063dd874ce8a166e89a2da9d049d4d895f
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2024-12-03 10:45:22 +0800

    Finally add a style to the error window

--- PenumbraModForwarder.UI/Views/ErrorWindow.axaml.cs
+++ PenumbraModForwarder.UI/Views/ErrorWindow.axaml.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Serilog;
 
 namespace PenumbraModForwarder.UI.Views;
 
@@ -9,5 +11,27 @@ public partial class ErrorWindow : Window
     public ErrorWindow()
     {
         InitializeComponent();
+        
+        var titleBar = this.FindControl<Grid>("TitleBar");
+        titleBar.PointerPressed += (s, e) =>
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                BeginMoveDrag(e);
+            }
+        };
+        
+        this.Get<Button>("CloseButton").Click += (s, e) =>
+        {
+            Log.Information("Close button clicked");
+            Close();
+        };
+    }
+
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        base.OnClosing(e);
+        Log.Information("Window closing");
+        Environment.Exit(0);
     }
 }
