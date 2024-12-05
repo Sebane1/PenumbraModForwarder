4e913ab35a89af0606bf2280337dedfa10ac5cf0
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2024-12-05 13:37:42 +0800

    Info boxes template working for HomeView

---
+++ PenumbraModForwarder.UI/Models/InfoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace PenumbraModForwarder.UI.Models;
+
+    public class InfoItem
+    {
+        public string Text { get; set; }
+        public int Number { get; set; }
+
+        public InfoItem(string text, int number)
+        {
+            Text = text;
+            Number = number;
+        }
+    }
--- PenumbraModForwarder.UI/ViewModels/HomeViewModel.cs
+++ PenumbraModForwarder.UI/ViewModels/HomeViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace PenumbraModForwarder.UI.ViewModels;
+﻿using System.Collections.ObjectModel;
+using PenumbraModForwarder.UI.Models;
+using ReactiveUI;
+
+namespace PenumbraModForwarder.UI.ViewModels;
 
 public class HomeViewModel : ViewModelBase
 {
+    private ObservableCollection<InfoItem> _infoItems;
+
+    public ObservableCollection<InfoItem> InfoItems
+    {
+        get => _infoItems;
+        set => this.RaiseAndSetIfChanged(ref _infoItems, value);
+    }
     
+    public HomeViewModel()
+    {
+        // Initialize with some sample data
+        InfoItems =
+        [
+            new("Item 1", 100),
+            new("Item 2", 200),
+            new("Item 3", 300)
+        ];
+    }
 }
