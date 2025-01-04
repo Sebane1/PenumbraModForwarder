df9e8b140b3311b1db5d58108a12adfa5ea22883
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-01-04 10:33:36 +0800

    Fix issues with changing download path in config

--- PenumbraModForwarder.Common/Services/ConfigurationService.cs
+++ PenumbraModForwarder.Common/Services/ConfigurationService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using KellermanSoftware.CompareNetObjects;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using PenumbraModForwarder.Common.Consts;
 using PenumbraModForwarder.Common.Events;
 using PenumbraModForwarder.Common.Interfaces;
@@ -125,17 +126,48 @@ public class ConfigurationService : IConfigurationService
         var properties = propertyPath.Split('.');
         object currentObject = obj;
         PropertyInfo propertyInfo = null;
+
         for (int i = 0; i < properties.Length; i++)
         {
             var propertyName = properties[i];
             propertyInfo = currentObject.GetType().GetProperty(propertyName);
+
             if (propertyInfo == null)
+            {
                 throw new Exception($"Property '{propertyName}' not found on type '{currentObject.GetType().Name}'");
+            }
+
             if (i == properties.Length - 1)
             {
-                // Convert the new value to the correct type
-                var convertedValue = Convert.ChangeType(newValue, propertyInfo.PropertyType);
-                propertyInfo.SetValue(currentObject, convertedValue);
+                // If the new value is a JArray, convert it to the desired type
+                if (newValue is JArray jArrayValue)
+                {
+                    // Handle List<string>:
+                    if (propertyInfo.PropertyType == typeof(List<string>))
+                    {
+                        var typedList = jArrayValue.ToObject<List<string>>();
+                        propertyInfo.SetValue(currentObject, typedList);
+                    }
+                    // Handle string[]:
+                    else if (propertyInfo.PropertyType.IsArray &&
+                             propertyInfo.PropertyType.GetElementType() == typeof(string))
+                    {
+                        var stringArray = jArrayValue.ToObject<string[]>();
+                        propertyInfo.SetValue(currentObject, stringArray);
+                    }
+                    else
+                    {
+                        // Generic fallback for other collection or array types
+                        var convertedCollection = jArrayValue.ToObject(propertyInfo.PropertyType);
+                        propertyInfo.SetValue(currentObject, convertedCollection);
+                    }
+                }
+                else
+                {
+                    // For non-JArray values, use standard type conversion
+                    var convertedValue = Convert.ChangeType(newValue, propertyInfo.PropertyType);
+                    propertyInfo.SetValue(currentObject, convertedValue);
+                }
             }
             else
             {
