1fd330d856b20a854ee1786d0be272d26282ed9d
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-01-04 21:58:21 +0800

    Code cleanup for ApplicationBootstrapper.cs

--- PenumbraModForwarder.Common/Services/ApplicationBootstrapper.cs
+++ PenumbraModForwarder.Common/Services/ApplicationBootstrapper.cs
@@ -1,27 +1,17 @@
-﻿namespace PenumbraModForwarder.Common.Services
+﻿namespace PenumbraModForwarder.Common.Services;
+
+public static class ApplicationBootstrapper
 {
-    public static class ApplicationBootstrapper
+    private static bool _isInitializedByWatchdog;
+    private static readonly bool _isDevMode;
+
+    static ApplicationBootstrapper()
     {
-        private static bool _isInitializedByWatchdog;
-        private static readonly bool _isDevMode;
+        _isDevMode = Environment.GetEnvironmentVariable("DEV_MODE") == "true";
+    }
 
-        static ApplicationBootstrapper()
-        {
-            _isDevMode = Environment.GetEnvironmentVariable("DEV_MODE") == "true";
-        }
-
-        public static void SetWatchdogInitialization()
-        {
-            _isInitializedByWatchdog = true;
-        }
-
-        public static bool IsInitializedByWatchdog()
-        {
-            if (_isDevMode)
-            {
-                return true;
-            }
-            return _isInitializedByWatchdog;
-        }
+    public static void SetWatchdogInitialization()
+    {
+        _isInitializedByWatchdog = true;
     }
 }
