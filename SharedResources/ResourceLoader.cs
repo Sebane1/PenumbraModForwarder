0a4c8626ebd9cb64c0b325ab48036c55a96cb148
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-01-07 00:31:31 +0800

    We now have icons YIPEE

---
+++ SharedResources/ResourceLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace SharedResources;
+
+public static class ResourceLoader
+{
+    public static Stream? GetResourceStream(string fileName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = assembly
+            .GetManifestResourceNames()
+            .FirstOrDefault(r => r.EndsWith(fileName, StringComparison.OrdinalIgnoreCase));
+
+        return resourceName == null ? null : assembly.GetManifestResourceStream(resourceName);
+    }
+}
