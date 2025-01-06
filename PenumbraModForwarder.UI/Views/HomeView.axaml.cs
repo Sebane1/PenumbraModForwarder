af083f188acf77b526f9f85c34ed23da99129c9b
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2025-01-06 11:10:07 +0800

    Click mod to view webpage (We love MVVM)

--- PenumbraModForwarder.UI/Views/HomeView.axaml.cs
+++ PenumbraModForwarder.UI/Views/HomeView.axaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using System;
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using PenumbraModForwarder.Common.Models;
 
 namespace PenumbraModForwarder.UI.Views;
 
@@ -14,5 +18,28 @@ public partial class HomeView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void OnItemPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        if (sender is Border border && border.DataContext is XmaMods mod)
+        {
+            var url = mod.ModUrl;
+            if (!string.IsNullOrWhiteSpace(url))
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = url,
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
     }
 }
