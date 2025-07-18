84278a50bd07cd41e4df5ad91ac6ae4968bfec26
Sebastina <sebane1@thavea.com>
2025-07-17 21:50:31 -0500

    Hard swap to using Atomos Mod Forwarder

--- PenumbraModForwarder.UI/Program.cs
+++ PenumbraModForwarder.UI/Program.cs
@@ -3,54 +3,48 @@ using PenumbraModForwarder.Common.Interfaces;
 using PenumbraModForwarder.UI.Interfaces;
 using PenumbraModForwarder.UI.Views;
 using Serilog;
+using SevenZipExtractor;
+using SharpCompress.Common.Zip;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+using System.Security.Policy;
 
 namespace PenumbraModForwarder.UI;
 
-static class Program
-{
+static class Program {
     private static IServiceProvider _serviceProvider;
 
     public static bool IsExiting { get; private set; } = false;
 
     [STAThread]
-    static void Main(string[] args)
-    {
+    static void Main(string[] args) {
         _serviceProvider = Extensions.ServiceExtensions.Configuration();
-
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-
-        if (args.Length > 0)
-        {
-            var filePath = args[0];
-            HandleFileArgs(filePath);
-            return;
-        }
-
-        Application.ApplicationExit += OnApplicationExit;
-
-        IsProgramAlreadyRunning();
-
-        CheckForUpdates();
-
-        if (IsExiting)
-        {
-            return;
-        }
-
         MigrateOldConfigIfExists();
-        CreateStartMenuShortcut();
-        SetTexToolsPath();
-        Application.Run(_serviceProvider.GetRequiredService<MainWindow>());
+        DownloadAndExtractAtomos();
+        SelfDestruct();
     }
-
-    private static void OnApplicationExit(object sender, EventArgs e)
-    {
+    public static void DownloadAndExtractAtomos() {
+        string downloadPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Atomos.zip");
+        string atomosPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Atomos.Launcher.exe");
+        using (var client = new WebClient()) {
+            client.DownloadFile("https://github.com/CouncilOfTsukuyomi/Atomos/releases/download/v1.2.7/Atomos-Windows-x64.v1.2.7.zip", downloadPath);
+        }
+        ZipFile.ExtractToDirectory(downloadPath, AppDomain.CurrentDomain.BaseDirectory, true);
+        File.Delete(downloadPath);
+        Process.Start(atomosPath);
+    }
+    public static void SelfDestruct() {
+        Process.Start(new ProcessStartInfo() {
+            Arguments = "/C choice /C Y /N /D Y /T 3 & Del \"" + Application.ExecutablePath + "\"",
+            WindowStyle = ProcessWindowStyle.Hidden, CreateNoWindow = true, FileName = "cmd.exe"
+        });
+    }
+    private static void OnApplicationExit(object sender, EventArgs e) {
         // Optional: Additional cleanup if needed
     }
 
-    public static void ExitApplication()
-    {
+    public static void ExitApplication() {
         IsExiting = true;
 
         Log.Information("Application is exiting.");
@@ -59,23 +53,19 @@ static class Program
         var fileHandlerService = _serviceProvider.GetRequiredService<IFileHandlerService>();
         fileHandlerService.CleanUpTempFiles();
 
-        if (_serviceProvider is IDisposable disposable)
-        {
+        if (_serviceProvider is IDisposable disposable) {
             disposable.Dispose();
         }
 
         Application.Exit();
     }
 
-    private static void HandleFileArgs(string filePath)
-    {
-        try
-        {
+    private static void HandleFileArgs(string filePath) {
+        try {
             Log.Information($"Running application with file: {filePath}");
 
             var allowedExtensions = new[] { ".pmp", ".ttmp2", ".ttmp" };
-            if (!allowedExtensions.Contains(Path.GetExtension(filePath)))
-            {
+            if (!allowedExtensions.Contains(Path.GetExtension(filePath))) {
                 Log.Error($"File '{filePath}' is not a valid mod file. Aborting.");
                 return;
             }
@@ -85,8 +75,7 @@ static class Program
             Log.Information("Starting mod installation...");
             var result = installService.InstallMod(filePath);
 
-            if (!result)
-            {
+            if (!result) {
                 Log.Error("Mod installation failed.");
                 return;
             }
@@ -99,41 +88,34 @@ static class Program
 
             Log.Information("Exiting application...");
             ExitApplication();
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Log.Error(ex, "An error occurred during the file handling process.");
             MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             ExitApplication();
         }
     }
 
-    private static void MigrateOldConfigIfExists()
-    {
+    private static void MigrateOldConfigIfExists() {
         var configurationService = _serviceProvider.GetRequiredService<IConfigurationService>();
         configurationService.MigrateOldConfig();
     }
 
-    private static void CheckForUpdates()
-    {
+    private static void CheckForUpdates() {
         var updateService = _serviceProvider.GetRequiredService<IUpdateService>();
         updateService.CheckForUpdates();
     }
 
-    private static void CreateStartMenuShortcut()
-    {
+    private static void CreateStartMenuShortcut() {
         var shortcutService = _serviceProvider.GetRequiredService<IShortcutService>();
         shortcutService.CreateShortcutInStartMenus();
     }
 
-    private static void SetTexToolsPath()
-    {
+    private static void SetTexToolsPath() {
         var texToolsHelper = _serviceProvider.GetRequiredService<ITexToolsHelper>();
         texToolsHelper.SetTexToolsConsolePath();
     }
 
-    private static void IsProgramAlreadyRunning()
-    {
+    private static void IsProgramAlreadyRunning() {
         var processHelperService = _serviceProvider.GetRequiredService<IProcessHelperService>();
         var result = processHelperService.IsApplicationAlreadyOpen();
         if (!result) return;
